527597464c5bd269039d589a48758346f3c01093
Julien Blondeau <julienblondeau@live.fr>
2020-12-06 14:01:27 +0100

    order page working + recap page

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Order;
+use App\Entity\OrderDetails;
 use App\Form\OrderType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +14,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/commande", name="order")
      */
@@ -24,15 +34,76 @@ class OrderController extends AbstractController
           'user' => $this->getUser()
         ]);
 
-        $form->handleRequest($request)
+        $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
-          
+
         }
 
         return $this->render('order/index.html.twig', [
           'form' => $form->createView(),
           'cart' => $cart->getFull()
         ]);
+    }
+
+    /**
+     * @Route("/commande/recapitulatif", name="order_recap", methods={"POST"})
+     */
+    public function add(Cart $cart, Request $request): Response
+    {
+
+        $form = $this->createForm(OrderType::class, null, [
+          'user' => $this->getUser()
+        ]);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+          $date = new \DateTime();
+          $carrier = $form->get('carriers')->getData();
+
+          $delivery = $form->get('addresses')->getData();
+          $delivery_content = $delivery->getFirstname().' '.$delivery->getLastname();
+          $delivery_content .= '<br/>'.$delivery->getPhone();
+
+          if($delivery->getCompany()) {
+            $delivery_content .= '<br/>'.$delivery->getCompany();
+          }
+
+          $delivery_content .= '<br/>'.$delivery->getAddress().' '.$delivery->getCity();
+          $delivery_content .= '<br/>'.$delivery->getCountry();
+
+          //Enregistrer la commande Order()
+          $order = new Order();
+          $order->setUser($this->getUser());
+          $order->setCreateAt($date);
+          $order->setCarrierName($carrier->getName());
+          $order->setCarrierPrice($carrier->getPrice());
+          $order->setDelivery($delivery_content);
+          $order->setIsPaid(false);
+
+          $this->entityManager->persist($order);
+
+          //Enregistrer les produits OrderDetails()
+          foreach($cart->getFull() as $product) {
+            $orderDetails = new OrderDetails();
+            $orderDetails->setMyOrder($order);
+            $orderDetails->setProduct($product['product']->getName());
+            $orderDetails->setQuantity($product['quantity']);
+            $orderDetails->setPrice($product['product']->getprice());
+            $orderDetails->setTotal($product['product']->getprice() * $product['quantity']);
+            $this->entityManager->persist($orderDetails);
+          }
+
+          //$this->entityManager->flush();
+
+          return $this->render('order/add.html.twig', [
+            'cart' => $cart->getFull(),
+            'carrier' => $carrier,
+            'delivery' => $delivery_content
+          ]);
+        }
+
+        return $this->redirectToRoute('cart');
     }
 }
